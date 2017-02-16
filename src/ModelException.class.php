5c4b8e5f7a27ae93c113fdb62507a56d74b9c593
RobinDumontChaponet <robin@dumontchapo.net>
2017-02-16 03:46:48 +0100

    Add basic ModelException.class

---
+++ src/ModelException.class.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Transitive\Utils;
+
+class ModelException extends \Exception
+{
+	public function getMessages(): array
+	{
+		$messages = [$this->getMessage()];
+
+		if($this->getPrevious())
+			$messages = array_merge($this->getPrevious()->getMessages(), $messages);
+
+		return $messages;
+	}
+
+	public function __toString(): string
+	{
+		$str = ''.$this->getPrevious().PHP_EOL;
+		$str.= ($str)?PHP_EOL:'';
+
+		$str.= $this->getMessage();
+
+		return $str;
+	}
+
+	public static function throw(self $e = null) {
+		if(isset($e))
+			throw $e;
+	}
+}
