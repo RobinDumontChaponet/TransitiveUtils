531a998172b09b397555c04389304b3301c596c0
RobinDumontChaponet <robin@dumontchapo.net>
2017-03-26 05:32:29 +0200

    php-cs-fixed

--- src/Arrays.class.php
+++ src/Arrays.class.php
@@ -4,30 +4,28 @@ namespace Transitive\Utils;
 
 abstract class Arrays
 {
-	/*
-	 * http://stackoverflow.com/questions/8321620/array-unique-vs-array-flip
+    /*
+     * http://stackoverflow.com/questions/8321620/array-unique-vs-array-flip
      */
-	public static function array_unique(array $array) : array
+    public static function array_unique(array $array): array
     {
         return array_flip(array_flip($array));
     }
 
-	public static function is_associative_array(array $array) : bool
+    public static function is_associative_array(array $array): bool
     {
         return sizeof($array) > 0 && array_keys($array) !== range(0, count($array) - 1);
     }
 
-	// does not keep keys…
-    public static function array_flatten(array $array) :  array
+    // does not keep keys…
+    public static function array_flatten(array $array): array
     {
         $flattened = array();
 
         array_walk_recursive($array, function ($value, $key) use (&$flattened) {
-			$flattened[] = $value;
+            $flattened[] = $value;
         });
 
         return $flattened;
     }
-
-
 }
--- src/ModelException.class.php
+++ src/ModelException.class.php
@@ -4,28 +4,28 @@ namespace Transitive\Utils;
 
 class ModelException extends \Exception
 {
-	public function getMessages(): array
-	{
-		$messages = [$this->getMessage()];
+    public function getMessages(): array
+    {
+        $messages = [$this->getMessage()];
 
-		if($this->getPrevious())
-			$messages = array_merge($this->getPrevious()->getMessages(), $messages);
+        if($this->getPrevious())
+            $messages = array_merge($this->getPrevious()->getMessages(), $messages);
 
-		return $messages;
-	}
+        return $messages;
+    }
 
-	public function __toString(): string
-	{
-		$str = ''.$this->getPrevious().PHP_EOL;
-		$str.= ($str)?PHP_EOL:'';
+    public function __toString(): string
+    {
+        $str = ''.$this->getPrevious().PHP_EOL;
+        $str .= ($str) ? PHP_EOL : '';
 
-		$str.= $this->getMessage();
+        $str .= $this->getMessage();
 
-		return $str;
-	}
+        return $str;
+    }
 
-	public static function throw(self $e = null) {
-		if(isset($e))
-			throw $e;
-	}
+    public static function throw(self $e = null) {
+        if(isset($e))
+            throw $e;
+    }
 }
